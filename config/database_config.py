64c11bfd8777ba7cdbb6b2fbae044755738d6f2d
horance0220 <horancelee0220.cs12@nycu.edu.tw>
2024-12-25 19:51:38 +0800

    Add files via upload

---
+++ config/database_config.py
@@ -0,0 +1,10 @@
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'dbuser',
+    'password': '0420',
+    'database': 'final',
+    'pool_name': 'mypool',
+    'pool_size': 5
+}
+
+SECRET_KEY = 'your-super-secret-key'  # 用於 Flask session
